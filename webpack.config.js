86965a2151da39416f1605d89bd2a172a5c379af
Chris Ball <chrisball02@gmail.com>
2018-08-21 22:18:31 +0200

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
-
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/index.js"],
+  entry: ["babel-polyfill", "./src/index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js"
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    quiet: true
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ template: "./src/index.html" }),
+    new FriendlyErrorsWebpackPlugin()
+  ]
+};
