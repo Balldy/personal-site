b613a1b10b7716c0bb54a6ea72936e3c40db4b07
Chris Ball <chrisball02@gmail.com>
2018-08-22 20:48:47 +0200

    update to font-awesome 5

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,16 +4,13 @@ import PropTypes from "prop-types";
 const Social = ({ className, iconSize }) => (
   <div className={className}>
     <a href="https://github.com/Balldy " target="blank">
-      <i className={`fa fa-github fa-${iconSize}x`} aria-hidden />
+      <i className={`fab fa-github fa-${iconSize}x`} aria-hidden />
     </a>
-    <a
-      href="https://es.linkedin.com/in/christopher-ball-548680a7 "
-      target="blank "
-    >
-      <i className={`fa fa-linkedin-square fa-${iconSize}x`} aria-hidden />
+    <a href="https://www.linkedin.com/in/chris-ball-548680a7/" target="blank ">
+      <i className={`fab fa-linkedin fa-${iconSize}x`} aria-hidden />
     </a>
     <a href="https://twitter.com/chrisballdy " target="blank">
-      <i className={`fa fa-twitter fa-${iconSize}x`} aria-hidden />
+      <i className={`fab fa-twitter fa-${iconSize}x`} aria-hidden />
     </a>
   </div>
 );
