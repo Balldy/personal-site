8640a1a991247ebe661003732d56b7642a162801
Chris Ball <chrisball02@gmail.com>
2018-08-26 16:38:13 +0200

    remove unecessary import

--- update-portfolio-lambda.py
+++ update-portfolio-lambda.py
@@ -1,11 +1,10 @@
 import boto3
-from botocore.client import Config
 import StringIO
 import zipfile
 import mimetypes
 
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+s3 = boto3.resource('s3')
 
 portfolio_bucket = s3.Bucket('portfolio.christopherball.info')
 build_bucket = s3.Bucket('portfoliobuild.christopherball.info')
