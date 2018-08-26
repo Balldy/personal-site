63dfb2d4c6efa87757ab6d05029635d249f128d7
Chris Ball <chrisball02@gmail.com>
2018-08-26 17:00:35 +0200

    update lambda

--- update-portfolio-lambda.py
+++ update-portfolio-lambda.py
@@ -3,20 +3,22 @@ import StringIO
 import zipfile
 import mimetypes
 
+def lambda_handler(event, context):
 
-s3 = boto3.resource('s3')
+    s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.christopherball.info')
-build_bucket = s3.Bucket('portfoliobuild.christopherball.info')
+    portfolio_bucket = s3.Bucket('portfolio.christopherball.info')
+    build_bucket = s3.Bucket('portfoliobuild.christopherball.info')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfolio.build.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfolio.build.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-print u'\u2713' + ' updated'
+    return 'updated portfolio'
+
