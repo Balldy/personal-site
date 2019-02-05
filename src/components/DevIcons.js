eaee1ddb1435b25a42a363d87e6f66648a2ed304
Chris Ball <chrisball02@gmail.com>
2019-02-05 22:18:07 +0100

    fix icons responsiveness

--- src/components/DevIcons.js
+++ src/components/DevIcons.js
@@ -3,15 +3,17 @@ import React from "react";
 const DevIcons = () => (
   <div className="dev-icons">
     <i className="icon-nodejs" />
+    <i className="icon-clojure" />
     <i className="icon-java-bold" />
-    <i className="icon-spring" />
+
+    <i className="icon-html5" />
+    <i className="icon-sass" />
     <i className="icon-reactjs" />
-    <i className="icon-playframework" />
-    <i className="icon-javascript" />
+
     <i className="icon-mongodb" />
     <i className="icon-postgres-alt" />
+
     <i className="icon-aws" />
-    <i className="icon-heroku" />
     <i className="icon-docker" />
   </div>
 );
