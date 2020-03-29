354e91109dc8b54b9414aaa50387127c07442204
Chris Ball <chrisball02@gmail.com>
2020-03-29 18:23:01 +0200

    Add social media links

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../Header";
+import Social from "../Social";
+
 import "./main.scss";
 
 const Main = () => (
   <div className="main">
     <Header />
+    <Social />
   </div>
 );
 
---
+++ src/components/Social/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./social.scss";
+
+const Social = () => (
+  <div className="social">
+    <a href="https://github.com/Balldy " target="blank">
+      github
+    </a>
+    <a href="https://www.linkedin.com/in/chris-ball-548680a7/" target="blank ">
+      linkedin
+    </a>
+    <a href="https://twitter.com/chrisballdy " target="blank">
+      twitter
+    </a>
+    <a className="email" href="mailto:chrisball02@gmail.com ">
+      e-mail
+    </a>
+  </div>
+);
+
+export default Social;
