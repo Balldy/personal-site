4d66ab9bc5e52c8fe450ef2bd04d45fdc1c7745e
Chris Ball <chrisball02@gmail.com>
2020-03-29 20:09:21 +0200

    Add about component

---
+++ src/components/About/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const About = ()=> {
+  return (
+    <div classname="about">
+      <p>
+       Hi, I'm Chris, I'm a software engineer, currently working at Thoughtworks in Barcelona.
+    </p>
+    </div>);
+
+}
+
+export default About;
