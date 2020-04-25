479c2f3e9b7fc51e51f93475df17a7d49c1150e9
Chris Ball <chrisball02@gmail.com>
2020-04-25 12:55:50 +0200

    Update text

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,10 +8,11 @@ const About = () => (
       Hi, I&apos;m Chris, I&apos;m a full stack developer working at&nbsp;
       <a href="https://www.thoughtworks.com/">Thoughtworks</a>
       &nbsp;in Barcelona. I always like trying new tech stacks but I&apos;m
-      usually happy working with Node.js, React, Clojure and AWS. Apart from
-      this I have experience with CI/CD, agile methodologies and I am very
-      passionate about infrastructure and helping teams grow a strong DevOps
-      culture. Outside of tech I also enjoy surfing and swimming in my freetime.
+      usually happy working with Node.js, React, Java or Clojure. As well as
+      this I have experience with CI/CD, agile methodologies, cloud
+      infrastructure and I am very passionate about growing a strong DevOps
+      culture on the teams I&apos;m part of. Outside of tech I like surfing and
+      swimming in my freetime.
     </p>
   </div>
 );
