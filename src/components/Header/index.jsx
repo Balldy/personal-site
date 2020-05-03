160d74dca8fc6d816bda11fd20ee2a0049dec12f
Chris Ball <chrisball02@gmail.com>
2020-05-03 18:21:59 +0200

    Use terminal icon svg

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import "./header.scss";
+import Terminal from "../../assets/icons/terminal.svg";
 
 const Header = () => (
   <div className="header">
     <h1 className="name">Christopher Ball</h1>
     <h2>
-      <span className="devicons devicons-terminal" />
+      <img src={Terminal} alt="terminal icon" />
       Software Engineer
     </h2>
   </div>
